22bb498f51c8358fae8f6afd6d568b5aba0a81d0
Daniel Tian <dtian.95@gmail.com>
2020-02-13 17:34:49 -0500

    added user signup, basic features

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 let User = require("../models/user.model");
 
 // testing
@@ -7,12 +8,28 @@ router.route("/").get((request, response) => {
 });
 
 // user signup
-router.route("/signup").post((request, response) => {
+router.route("/signup").post(async (request, response) => {
   const username = request.body.username;
-  const newUser = new User({ username });
+  const email = request.body.email;
+  const password = request.body.password;
+  const friends = [];
+  const meals = [];
+
+  const newUser = new User({
+    username,
+    email,
+    password,
+    friends,
+    meals
+  });
+
+  const salt = await bcrypt.genSalt(10);
+
+  newUser.password = await bcrypt.hash(password, salt);
+
   newUser
     .save()
-    .then(() => response.json("User Sign Up!"))
+    .then(data => response.json(data))
     .catch(error => response.status(400).json("Error: " + error));
 });
 
@@ -23,12 +40,18 @@ router.route("/login").get((request, response) => {
 
 // add new friends phone number / contact
 // make sure user only has 10 friends at once
-router.route("/new/contact").post((request, response) => {
+router.route("/friend").post((request, response) => {
+  const uid = request.body.uid;
+  const name = request.body.name;
+  const number = request.body.number;
+
+  const newFriend = { name, number };
+
   response.send("create new contact");
 });
 
 // delete friends phone number / contact
-router.route("/delete/contact").post((request, response) => {
+router.route("/friend/:id").delete((request, response) => {
   response.send("delete contact");
 });
 
