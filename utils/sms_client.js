2f68b4125bdfbaaeed6c61c6c6ac996697e55a68
Daniel Tian <dtian.95@gmail.com>
2020-02-22 19:10:26 -0500

    fixed env file

--- utils/sms_client.js
+++ utils/sms_client.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "../.env" });
 const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWIOLIO_AUTH_TOKEN;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
 // messege is a string "hello world"
